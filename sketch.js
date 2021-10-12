a8e2badf38bff3d87d672dc189af504e18cf5bbe
deron6 <91129613+deron6@users.noreply.github.com>
2021-10-12 11:57:42 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,6 @@ let wallLeft;
 let wallRight;
 let ground;
 
-
 let T1L = {
   colorR: 255,
   colorG: 0,
@@ -46,8 +45,8 @@ let T1R= {
 
 let T2L= {
   angle: 90,
-  colorR: 255,
-  colorG: 0,
+  colorR: 0,
+  colorG: 255,
   colorB: 0,
   x1: 300,
   y1: 100,
@@ -71,7 +70,8 @@ let T2R = {
   y3: 300,
   speed: 1
 }
-
+let colorChange= 1
+let T2L_color= (T2L.colorR, T2L.colorG,T2L.colorB)
 let rotateObjectsArray= [T1L,T2L];
 let speedObjectsArray02 = [T1R,T2R];
 let tcolorArray= [T1L.colorR,T1L.colorG,T1L.colorB]
@@ -127,6 +127,11 @@ function setup() {
   mouseConstraint.mouse.pixelRatio = pixelDensity();
   World.add(engine.world, mouseConstraint);
 
+
+if (triangleArrayBodies[i]>700 || (triangleArray.colorR,triangleArray.colorG,triangleArray.colorB)<colorChange){colorChange = -colorChange}
+
+
+
   // run the engine
   Engine.run(engine);
 }
@@ -134,12 +139,22 @@ function setup() {
 function draw() {
   background(0);
 
+
+  if (T2L_color > 255 || T2L_color < 0) {
+    colorChange = -colorChange;
+  }
+  T2L_color = T2L_color + colorChange;
+  console.log(T2L_color)
+
+
   noStroke();
-  fill(255);
 
   for (i = 0; i < triangleArray.length; i++) {
+    fill(triangleArray[i].colorR, triangleArray[i].colorG, triangleArray[i].colorB);
     drawBody(triangleArrayBodies[i]);
   };
+
+  
 
   fill(0);
   drawBody(ground);
